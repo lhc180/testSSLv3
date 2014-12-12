0dd3698b7712c0f6cd801508f36c7840c2e8606c
Steve Lhomme <robux4@gmail.com>
2014-12-12 11:46:44 +0100

    manually force Conscrypt otherwise it's not used

--- src/main/java/org/robux4/testionsslv3/MainActivity.java
+++ src/main/java/org/robux4/testionsslv3/MainActivity.java
@@ -1,6 +1,7 @@
 package org.robux4.testionsslv3;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
@@ -8,11 +9,14 @@ import android.widget.ImageView;
 import com.koushikdutta.async.http.AsyncSSLEngineConfigurator;
 import com.koushikdutta.ion.Ion;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLParameters;
 
 
 public class MainActivity extends Activity {
@@ -24,7 +28,7 @@ public class MainActivity extends Activity {
 
 		Ion.getDefault(this).getConscryptMiddleware().enable(false);
 
-		if (false) {
+		if (true) {
 			// disable SSLv3 unless that's the only protocol the engine can handle
 			Ion.getDefault(this).getHttpClient().getSSLSocketMiddleware().addEngineConfigurator(new AsyncSSLEngineConfigurator() {
 				@Override
@@ -37,8 +41,73 @@ public class MainActivity extends Activity {
 							engine.setEnabledProtocols(protocols);
 						}
 					}
+/*
+					SSLParameters sslp = engine.getSSLParameters();
+					try {
+						//Field sslParameters = engine.getClass().getDeclaredField("sslParameters");
+						Field useSni = sslp.getClass().getDeclaredField("useSni");
+						Field peerHost = engine.getClass().getSuperclass().getDeclaredField("peerHost");
+						Field peerPort = engine.getClass().getSuperclass().getDeclaredField("peerPort");
+
+						peerHost.setAccessible(true);
+						peerPort.setAccessible(true);
+						//sslParameters.setAccessible(true);
+						useSni.setAccessible(true);
+
+						//Object sslp = sslParameters.get(engine);
+
+						peerHost.set(engine, host);
+						peerPort.set(engine, port);
+						useSni.set(sslp, true);
+
+						engine.setSSLParameters(sslp);
+					} catch (Exception e) {
+					}
+*/
 				}
 			});
+		}
+/*
+		Ion.getDefault(this).getHttpClient().getSSLSocketMiddleware().addEngineConfigurator(new AsyncSSLEngineConfigurator() {
+			@Override
+			public void configureEngine(SSLEngine engine, String host, int port) {
+				try {
+					Field sslParameters = engine.getClass().getDeclaredField("sslParameters");
+					Field useSni = sslParameters.getType().getDeclaredField("useSni");
+					Field peerHost = engine.getClass().getSuperclass().getDeclaredField("peerHost");
+					Field peerPort = engine.getClass().getSuperclass().getDeclaredField("peerPort");
+
+					peerHost.setAccessible(true);
+					peerPort.setAccessible(true);
+					sslParameters.setAccessible(true);
+					useSni.setAccessible(true);
+
+					Object sslp = sslParameters.get(engine);
+
+					peerHost.set(engine, host);
+					peerPort.set(engine, port);
+					useSni.set(sslp, true);
+
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+*/
+		try {
+			Class<?> providerInstaller = Class.forName("com.google.android.gms.security.ProviderInstaller");
+			Method mInsertProvider = providerInstaller.getDeclaredMethod("installIfNeeded", Context.class);
+			mInsertProvider.invoke(null, this);
+
+		} catch (Throwable ignored) {
+			try {
+				Context gms = createPackageContext("com.google.android.gms", Context.CONTEXT_INCLUDE_CODE | Context.CONTEXT_IGNORE_SECURITY);
+				Class clazz = gms.getClassLoader().loadClass("com.google.android.gms.common.security.ProviderInstallerImpl");
+				Method mInsertProvider = clazz.getDeclaredMethod("insertProvider", Context.class);
+				mInsertProvider.invoke(null, this);
+			} catch (Throwable e) {
+			}
 		}
 
 		Ion.with((ImageView) findViewById(R.id.imageView))
