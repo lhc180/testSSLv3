4156afc0a2158626b52dff7e52b5b2fac1501085
Steve Lhomme <robux4@gmail.com>
2014-12-15 10:47:46 +0100

    Ion 2.0 initializes Conscrypt correctly, unlike Ion 1.4.1

--- src/main/java/org/robux4/testionsslv3/MainActivity.java
+++ src/main/java/org/robux4/testionsslv3/MainActivity.java
@@ -30,9 +30,11 @@ import javax.net.ssl.SSLEngine;
 
 public class MainActivity extends Activity {
 
-	private static final boolean FORCE_USE_SNI = true; // dual call fails when we for Conscrypt to use SNI
+	private static final boolean USE_CONSCRYPT = true;
+	private static final boolean FORCE_USE_SNI = false; // dual call fails when we for Conscrypt to use SNI
 	private static final boolean DISABLE_SSLV3 = false;
 	private static final boolean DEBUG_ION = false;
+
 	private static final int DELAY_BETWEEN_CALLS = 200; // ms
 
 	private final static String FEEDLY_DOMAIN = "sandbox.feedly.com";
@@ -57,7 +59,7 @@ public class MainActivity extends Activity {
 		} catch (PackageManager.NameNotFoundException ignored) {
 		}
 
-		Ion.getDefault(this).getConscryptMiddleware().enable(false);
+		Ion.getDefault(this).getConscryptMiddleware().enable(USE_CONSCRYPT);
 
 		Ion.getDefault(this).getHttpClient().getSSLSocketMiddleware().addEngineConfigurator(new AsyncSSLEngineConfigurator() {
 			@Override
@@ -107,6 +109,7 @@ public class MainActivity extends Activity {
 			}
 		});
 
+		/*
 		// install Conscrypt ourselves as 1.4.1 doesn't set it right
 		try {
 			Class<?> providerInstaller = Class.forName("com.google.android.gms.security.ProviderInstaller");
@@ -121,7 +124,7 @@ public class MainActivity extends Activity {
 				mInsertProvider.invoke(null, this);
 			} catch (Throwable e) {
 			}
-		}
+		}*/
 
 		Ion.with((ImageView) findViewById(R.id.imageView))
 				.load("http://cdn2.vox-cdn.com/thumbor/KxtZNw37jKNfxdA0hX5edHvbTBE=/0x0:2039x1359/800x536/cdn0.vox-cdn.com/uploads/chorus_image/image/44254028/lg-g-watch.0.0.jpg");
